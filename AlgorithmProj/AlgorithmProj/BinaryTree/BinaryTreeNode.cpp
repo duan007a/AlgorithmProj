171d53e846b764dd7a3cfe63e41c8e4c2bb518ae
runsheng <runsheng@mogujie.com>
2017-07-17 14:51:38 +0800

    二叉树

---
+++ AlgorithmProj/AlgorithmProj/BinaryTree/BinaryTreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTreeNode.cpp
+//  AlgorithmProj
+//
+//  Created by runsheng on 2017/7/17.
+//  Copyright © 2017年 段洪春. All rights reserved.
+//
+
+#include "BinaryTreeNode.hpp"
---
+++ AlgorithmProj/AlgorithmProj/BinaryTree/BinaryTreeNode.hpp
@@ -0,0 +1,26 @@
+//
+//  BinaryTreeNode.hpp
+//  AlgorithmProj
+//
+//  Created by runsheng on 2017/7/17.
+//  Copyright © 2017年 段洪春. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_hpp
+#define BinaryTreeNode_hpp
+
+#include <stdio.h>
+
+struct BinaryTreeNode {
+    int m_nValue;
+    BinaryTreeNode *m_pLeft;
+    BinaryTreeNode *m_pRight;
+};
+
+class BinaryTreeNodeDemo {
+    
+public:
+    
+};
+
+#endif /* BinaryTreeNode_hpp */
